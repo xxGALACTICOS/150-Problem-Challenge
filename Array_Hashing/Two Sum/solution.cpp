a5d7a8e00a4cb95e4987547a401e4c83b54e9560
ahmedhamdiola <144958536+ahmedhamdiola@users.noreply.github.com>
2025-03-15 00:34:38 +0200

    Hamdiola

---
+++ Array_Hashing/Two Sum/solution.cpp
@@ -0,0 +1,17 @@
+                                            //  -- Two Sum -- \\
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++) {
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums[i] + nums[j] == target && i != j) {
+                result.push_back(i);
+                result.push_back(j);
+                return result;
+            }
+        }
+        }
+    }
+};
