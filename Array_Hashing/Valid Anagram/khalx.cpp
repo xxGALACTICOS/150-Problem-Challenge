eaad35baa1e91ef662f8c564aa92ac4ddcd4239f
ahmedkhalx <167723235+ahmedkhalx@users.noreply.github.com>
2025-03-15 16:52:49 +0200

    khalx.cpp

--- Array_Hashing/Valid Anagram/khalx.cpp
+++ Array_Hashing/Valid Anagram/khalx.cpp
@@ -1,10 +1,8 @@
-#include<iostream>
-#include<algorithm>
-#include<string>
-using namespace std;
 
-
-bool isAnagram(string s, string t) {
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        	
 	sort(s.begin(), s.end());
 	sort(t.begin(), t.end());
 
@@ -12,4 +10,5 @@ bool isAnagram(string s, string t) {
 		return true;
 	}
 	return false;
-}
+    }
+};
