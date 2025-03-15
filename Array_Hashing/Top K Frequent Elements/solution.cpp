8576e005234beb33e552d96eb8c9200a483f4ac8
ahmedhamdiola <144958536+ahmedhamdiola@users.noreply.github.com>
2025-03-15 03:19:37 +0200

    Hamdiola

---
+++ Array_Hashing/Top K Frequent Elements/solution.cpp
@@ -0,0 +1,28 @@
+                                      //-- Top K Frequent Elements --\\
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> v;
+        map<int, int> freq;
+        for (int i : nums) {
+            freq[i]++;
+        }
+        for (int i = 0; i < k; i++) {
+            int temp = -1;
+            int highest;
+            pair<const int,int>* ptr = NULL;
+            for (auto j = freq.begin(); j != freq.end(); j++) {
+                int value = j->second;
+                if (value > temp) {
+                    temp = value;
+                    highest = j->first;
+                    ptr = &(*j);
+                }
+            }
+            v.push_back(ptr->first);
+            ptr->second = -1;
+        }
+        return v;
+    }
+};
