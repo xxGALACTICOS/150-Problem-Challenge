025c8a47e47db5439dd4a7f6b466b7f40f254ec0
ahmedhamdiola <144958536+ahmedhamdiola@users.noreply.github.com>
2025-03-22 01:32:16 +0200

    Hamdiola

---
+++ Array_Hashing/Encode and Decode Strings/solution.cpp
@@ -0,0 +1,35 @@
+					    // -- Encode and Decode Strings -- \\
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string s;
+        for (int i = 0; i < strs.size(); i++) {
+           s.append(strs[i]);
+           s.append("~");
+        }
+        return s;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> res;
+        vector<char> v;
+        int i = 0;
+        while (i < s.size()) {
+            v.push_back(s.at(i));
+            i++;
+        }
+
+        string st;
+        for (int i = 0; i < v.size(); i++) {
+            if (v[i] == '~' || i == v.size() - 1) {
+                res.push_back(st);
+                st = "";
+                continue;
+            }
+            st.push_back(v[i]);
+        }
+        return res;
+    }
+
+};
