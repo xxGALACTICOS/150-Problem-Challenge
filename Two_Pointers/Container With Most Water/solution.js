65afe6a4d6397eb80c7767af1abbcb395e510e33
ahmed ashraf <160669170+AhmedAshraf780@users.noreply.github.com>
2025-03-31 02:39:00 +0200

    still bad

--- Two_Pointers/Container With Most Water/solution.js
+++ Two_Pointers/Container With Most Water/solution.js
@@ -1,1 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
 
+        var left = 0, right = heights.length-1;
+        var maxA = 0;
+
+        while(left < right){
+            var height = Math.min(heights[left],heights[right]);
+            var len =  Math.abs(right - left);
+
+            maxA = Math.max(maxA , height*len);
+
+            if(heights[right] >= heights[left]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        
+        return maxA;
+    }
+}
